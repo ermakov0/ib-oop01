152a6e06ecc65f32c928eaf3aef56a381063f125
ermakov0 <tour0@ya.ru>
2024-10-03 13:21:26 +0700

    02

---
+++ 02-dotnet-struct/PointStruct.cs
@@ -0,0 +1,34 @@
+﻿namespace _02_dotnet_struct
+{
+    public struct PointStruct
+    {
+        /// <summary>
+        /// Вместо конструктора с параметрами
+        /// </summary>
+        /// <param name="x">Координата X</param>
+        /// <param name="y">Координата Y</param>
+        /// <param name="z">Координата Z</param>
+        public void Init(double x, double y, double z) { X = x; Y = y; Z = z; }
+
+        /// <summary>
+        /// Получить строковое представление объекта
+        /// </summary>
+        /// <returns>Строковое представление объекта</returns>
+        public override string ToString() { return "PointStruct(" + X + "," + Y + "," + Z + ")"; }
+
+        /// <summary>
+        /// Координата X
+        /// </summary>
+        public double X { get; set; }
+
+        /// <summary>
+        /// Координата Y
+        /// </summary>
+        public double Y { get; set; }
+
+        /// <summary>
+        /// Координата Z
+        /// </summary>
+        public double Z { get; set; }
+    }
+}
---
+++ 02-dotnet-struct/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Разница между class и struct 
+*/
+namespace _02_dotnet_struct
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Point point = new Point();
+            point.Init(1.0, 2.0, 3.0);
+            Point point2 = point;
+            Console.WriteLine(point + "; " + point2);
+            point.X = 0.0;
+            Console.WriteLine(point + "; " + point2);
+
+            PointStruct pointStr = new PointStruct();
+            pointStr.Init(1.0, 2.0, 3.0);
+            PointStruct pointStr2 = pointStr;
+            Console.WriteLine(pointStr + "; " + pointStr2);
+            pointStr.X = 0.0;
+            Console.WriteLine(pointStr + "; " + pointStr2);
+        }
+    }
+}
+
